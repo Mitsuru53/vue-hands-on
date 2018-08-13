666be6179cf2305272157cf60ae3531dc1843128
Mitsuru Hashimoto <mitsuruhashimoto@MitsurunoMacBook-puro.local>
2018-08-13 14:05:31 +0900

    Vue basic feature

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div id="app" >
+    <div class="container">
+      <Header />
+      <form @submit.prevent="authSubmit" class="form-group">
+        <input type="email" v-model="email" class="form-control"/> email: {{ email }}
+        <br>
+        <input type="password" v-model="password" class="form-control"/> password: {{ password }}
+        <br>
+        <input type="submit" value="submit" class="btn btn-success"/>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Header from './components/Header.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+  },
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    authSubmit () {
+      console.log(this.email, this.password)
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <b-nav>
+    <b-nav-item active>Active</b-nav-item>
+    <b-nav-item>Login</b-nav-item>
+    <b-nav-item disabled>Disabled</b-nav-item>
+  </b-nav>
+</template>
